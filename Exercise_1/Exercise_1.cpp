12a0518f9dbcfdd1545beb29ed2ef2f482b3e1fd
Stewart <smh@smh.laptop>
2017-10-17 14:47:25 +0100

    Add new exercise 1

--- Exercise_1/Exercise_1.cpp
+++ Exercise_1/Exercise_1.cpp
@@ -1,43 +1,41 @@
-#include <random>
 #include <iostream>
-#include <chrono>
-#include <cmath>
+#include <vector>
 
-// Calculate value of pi using Monte Carlo integration
-// Points within circle of radius 1 inscribed in 2x2 square in ratio pi/4
-// Monte Carlo integration converges to correct answer (slowly!)
-// Compile with
-// g++ Exercise_1.cpp -std=c++14 -OX -g -o Exercise_1
-// or
-// clang Exercise_1.cpp -std=c++14 -OX -g -lstdc++ -lm -o Exercise_1
-// where X goes from 0 to 3
-// Time and profile your output
-// time ./Exercise_1
-// valgrind --tool=cachegrind ./Exercise_1
+bool isPrime(unsigned int n) {
+  for (unsigned int i = 2; i < n; i++) {
+    if (n%i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 
 int main() {
 
-
-  //initialisation
-  std::default_random_engine generator;
-  std::uniform_real_distribution<double> distribution(-1,1);
-
-  unsigned int max = 10000000;
-  unsigned int in_circle = 0;
-  //main loop
-  for (unsigned int i = 0; i < max; ++i) {
-    double x = distribution(generator);
-    double y = distribution(generator);
-    double r = std::sqrt(x*x + y*y);
-    if (r < 1) { 
-      in_circle++;
+  unsigned int max = 1000000;
+  std::vector<unsigned int> primes;
+  for (unsigned int i = 2; i < max; ++i) {
+    if (isPrime(i)) {
+      primes.push_back(i); 
+      //std::cout << i << std::endl;
     }
   }
-
-  //output
-  double pi = 4*in_circle/double(max);
-  std::cout << pi << std::endl;
+  //for (unsigned int prime : primes) {
+  //  std::cout << prime << std::endl;
+  //}
+  std::cout << "There are " << primes.size() << "primes below " << max << std::endl;
 
   return 0;
-
 }
+
+
+
+
+
+
+
+
+
+
+
