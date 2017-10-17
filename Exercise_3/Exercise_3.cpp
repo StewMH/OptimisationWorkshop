19f163e719bd84dbefc10fbd71d0154f32ddecbf
Stewart <smh@smh.laptop>
2017-10-17 17:33:28 +0100

    Allocation example

---
+++ Exercise_3/Exercise_3.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+
+struct A 
+{
+ int x;
+ int y;
+ int z;
+};
+
+
+int main() {
+
+  unsigned int max = 10000000;
+  std::vector<A> vec_A;
+  for (unsigned int i = 0; i < max; i++) {
+    A i_A;
+    i_A.x = i; 
+    i_A.y = i+1; 
+    i_A.z = i+2; 
+    vec_A.push_back(i_A);
+  }
+  std::cout << vec_A.size() << std::endl;
+}
