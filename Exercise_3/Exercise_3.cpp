fefd1a744fc0944a24254fa7ae467687648994c5
Stewart <smh@smh.laptop>
2017-10-19 16:13:07 +0100

    Stick to 1 G memory

--- Exercise_3/Exercise_3.cpp
+++ Exercise_3/Exercise_3.cpp
@@ -12,26 +12,25 @@
 // valgrind --tool=callgrind ./Exercise_3
 
 //A simple data object containing three ints
-struct A 
+
+struct A
 {
  int x;
  int y;
  int z;
 };
 
-
 int main() {
 
-  unsigned int max = 100000000;
+  unsigned int max = 10000000;
   std::vector<A*> vec_A;
+  //vec_A.reserve(max); //request all the space needed in advance
   for (unsigned int i = 0; i < max; i++) {
     A* i_A = new A();
-    i_A->x = i; 
-    i_A->y = i+1; 
-    i_A->z = i+2; 
+    i_A->x = i;
+    i_A->y = i+1;
+    i_A->z = i+2;
     vec_A.push_back(i_A);
-    //std::cout << vec_A.size() << std::endl;
-    //std::cout << vec_A.capacity() << std::endl;
   }
-  //std::cout << vec_A.size() << std::endl;
+  std::cout << vec_A.size() << std::endl;
 }
