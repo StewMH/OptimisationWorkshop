959ae7f35feb112133a4d4628ab0f54543b8d2d8
Stewart <smh@smh.laptop>
2017-10-19 23:33:41 +0100

    Elements on stack

--- Exercise_3/Exercise_3.cpp
+++ Exercise_3/Exercise_3.cpp
@@ -23,13 +23,13 @@ struct A
 int main() {
 
   unsigned int max = 10000000;
-  std::vector<A*> vec_A;
+  std::vector<A> vec_A;
   //vec_A.reserve(max); //request all the space needed in advance
   for (unsigned int i = 0; i < max; i++) {
-    A* i_A = new A();
-    i_A->x = i;
-    i_A->y = i+1;
-    i_A->z = i+2;
+    A i_A;
+    i_A.x = i;
+    i_A.y = i+1;
+    i_A.z = i+2;
     vec_A.push_back(i_A);
   }
   std::cout << vec_A.size() << std::endl;
